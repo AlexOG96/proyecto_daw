5e05ec189ac9d57c7763951227cc8606e662566d
AlexOG96 <45387397+AlexOG96@users.noreply.github.com>
2020-03-26 18:53:40 +0100

    BBDD 1.0

---
+++ novagames.sql
@@ -0,0 +1,120 @@
+-- phpMyAdmin SQL Dump
+-- version 5.0.1
+-- https://www.phpmyadmin.net/
+--
+-- Servidor: 127.0.0.1
+-- Tiempo de generación: 25-03-2020 a las 17:51:29
+-- Versión del servidor: 10.4.11-MariaDB
+-- Versión de PHP: 7.4.1
+
+SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+SET AUTOCOMMIT = 0;
+START TRANSACTION;
+SET time_zone = "+00:00";
+
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8mb4 */;
+
+--
+-- Base de datos: `novagames`
+--
+
+-- --------------------------------------------------------
+
+--
+-- Estructura de tabla para la tabla `adivinanumero`
+--
+
+CREATE TABLE `adivinanumero` (
+  `ID` int(11) NOT NULL,
+  `dificultad` varchar(255) NOT NULL,
+  `puntuacion` int(11) NOT NULL DEFAULT 0
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+-- --------------------------------------------------------
+
+--
+-- Estructura de tabla para la tabla `parejas`
+--
+
+CREATE TABLE `parejas` (
+  `ID` int(11) NOT NULL,
+  `dificultad` varchar(255) NOT NULL,
+  `puntuacion` int(11) NOT NULL DEFAULT 0
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+-- --------------------------------------------------------
+
+--
+-- Estructura de tabla para la tabla `usuarios`
+--
+
+CREATE TABLE `usuarios` (
+  `ID` int(11) NOT NULL,
+  `nombre` varchar(255) NOT NULL,
+  `contrasenna` varchar(255) NOT NULL,
+  `fecha_nacimiento` date NOT NULL,
+  `novacoins` int(11) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+--
+-- Índices para tablas volcadas
+--
+
+--
+-- Indices de la tabla `adivinanumero`
+--
+ALTER TABLE `adivinanumero`
+  ADD PRIMARY KEY (`ID`);
+
+--
+-- Indices de la tabla `parejas`
+--
+ALTER TABLE `parejas`
+  ADD PRIMARY KEY (`ID`);
+
+--
+-- Indices de la tabla `usuarios`
+--
+ALTER TABLE `usuarios`
+  ADD PRIMARY KEY (`ID`);
+
+--
+-- AUTO_INCREMENT de las tablas volcadas
+--
+
+--
+-- AUTO_INCREMENT de la tabla `parejas`
+--
+ALTER TABLE `parejas`
+  MODIFY `ID` int(11) NOT NULL AUTO_INCREMENT;
+
+--
+-- AUTO_INCREMENT de la tabla `usuarios`
+--
+ALTER TABLE `usuarios`
+  MODIFY `ID` int(11) NOT NULL AUTO_INCREMENT;
+
+--
+-- Restricciones para tablas volcadas
+--
+
+--
+-- Filtros para la tabla `parejas`
+--
+ALTER TABLE `parejas`
+  ADD CONSTRAINT `parejas_ibfk_1` FOREIGN KEY (`ID`) REFERENCES `adivinanumero` (`ID`);
+
+--
+-- Filtros para la tabla `usuarios`
+--
+ALTER TABLE `usuarios`
+  ADD CONSTRAINT `usuarios_ibfk_1` FOREIGN KEY (`ID`) REFERENCES `parejas` (`ID`);
+COMMIT;
+
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
